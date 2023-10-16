48eddc36be8b52be94fb2928016c6b188bac1203
Felipe S. Oliver <85701952+felipesantosdd@users.noreply.github.com>
2023-10-16 19:23:22 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,11 @@
-// import { z } from 'zod'
-
-// const clientSchema = z.object({
-//     name: z.string().min(3, { message: 'O usuario precisa de um nome' })
-// })
-
-// type Client = z.infer<typeof clientSchema>
-
-// export function print(client: Client) {
-//     console.log('Oii ' + client.name)
-//     return 'Oii ' + client.name
-// }
-// const client = { name: 'Felipe' }
-// print(client)
-
 import 'reflect-metadata';
 import app from "./app";
 import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize()
     .then(() => {
-        // const PORT: number = Number(process.env.PORT || 3000);
-        const PORT: number = 3001;
+        const PORT: number = Number(process.env.PORT || 5432);
+        
         app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
     })
     .catch((error) => console.error(error));
