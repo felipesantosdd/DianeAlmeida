fbeb039b9a84d562a57db943fabf6835c210b330
Felipe S. Oliver <85701952+felipesantosdd@users.noreply.github.com>
2023-10-16 17:40:01 -0300

    Update S3Storage.ts

--- src/utils/S3Storage.ts
+++ src/utils/S3Storage.ts
@@ -1,58 +1,56 @@
-import AWS from "aws-sdk";
+import { S3 } from "@aws-sdk/client-s3";
 import path from "path";
 import mime from "mime";
-import multerConfig from '../config/multer'
 import { AppError } from "../error/error";
-import fs from "fs";
+import { promises as fsPromises } from "fs";
 import "dotenv/config";
 
 class S3Storage {
-    private client: AWS.S3
+    private client: S3;
 
     constructor() {
-        this.client = new AWS.S3({
-            accessKeyId: process.env.AWS_ACECESS_KEY,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        this.client = new S3({
             region: 'us-east-2',
-        })
+            credentials: {
+                accessKeyId: process.env.AWS_ACECESS_KEY,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            },
+        });
     }
 
     async saveFile(fileName: string): Promise<void> {
         const originalPath = path.resolve(`src/tmp/${fileName}`);
-
         const contentType = mime.getType(originalPath);
 
         if (!contentType) {
             throw new AppError('Tipo de conteúdo não encontrado para o arquivo', 404);
         }
 
-        const fileContent = await fs.promises.readFile(originalPath);
+        const fileContent = await fsPromises.readFile(originalPath);
 
-        await this.client.putObject({
-            Bucket: `dianealmeida-modelos`,
+        await this.client.send(new PutObjectCommand({
+            Bucket: 'dianealmeida-modelos',
             Key: fileName,
             Body: fileContent,
             ContentType: contentType,
-            ACL: 'public-read'
-        }).promise();
-
-        await fs.promises.unlink(originalPath);
+            ACL: 'public-read',
+        }));
+        
+        await fsPromises.unlink(originalPath);
     }
 
     async getFile(fileName: string): Promise<Buffer> {
-        const response = await this.client.getObject({
+        const response = await this.client.send(new GetObjectCommand({
             Bucket: 'dianealmeida-modelos',
             Key: fileName,
-        }).promise();
-
+        }));
 
         if (response.Body) {
-            return response.Body as Buffer;
+            return response.Body;
         } else {
             throw new AppError('Arquivo não encontrado no S3', 404);
         }
     }
 }
-
 
 export default S3Storage;
